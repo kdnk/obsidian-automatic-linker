80de9370ddbbd5a0209cec8db13e9a05748d172c
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2025-02-04 18:24:33 +0900

    chore: save and modify links in the save command

--- src/main.ts
+++ src/main.ts
@@ -178,8 +178,8 @@ export default class AutomaticLinkerPlugin extends Plugin {
 				{ leading: true },
 			);
 			saveCommandDefinition.callback = async () => {
-				save?.();
-				throttledModifyLinks();
+				await throttledModifyLinks();
+				await save?.();
 			};
 		}
 	}
