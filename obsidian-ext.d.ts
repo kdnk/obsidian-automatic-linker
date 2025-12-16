dc292f1424dddf91a97542050553f6662a862ceb
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2025-12-16 22:36:54 +0900

    chore: add obsidian-ext.d.ts for extended type definitions

---
+++ obsidian-ext.d.ts
@@ -0,0 +1,23 @@
+import 'obsidian';
+import { EditorView } from '@codemirror/view';
+
+declare module 'obsidian' {
+  interface App {
+    commands: {
+      commands: {
+        'editor:save-file': {
+          callback?: () => void;
+          checkCallback?: (checking: boolean) => void;
+        };
+      };
+    };
+  }
+
+  interface Editor {
+    cm?: EditorView;
+  }
+
+  interface Vault {
+    getConfig(id: string): string;
+  }
+}
