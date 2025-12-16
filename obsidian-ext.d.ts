32dbe2edcedd879c3de4e96ff5797ed84e3c33b0
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2025-12-16 23:40:29 +0900

    chore: tabsize set to 4 in update-editor.ts

--- obsidian-ext.d.ts
+++ obsidian-ext.d.ts
@@ -1,23 +1,23 @@
-import 'obsidian';
-import { EditorView } from '@codemirror/view';
+import "obsidian";
+import { EditorView } from "@codemirror/view";
 
-declare module 'obsidian' {
-  interface App {
-    commands: {
-      commands: {
-        'editor:save-file': {
-          callback?: () => void;
-          checkCallback?: (checking: boolean) => void;
-        };
-      };
-    };
-  }
+declare module "obsidian" {
+	interface App {
+		commands: {
+			commands: {
+				"editor:save-file": {
+					callback?: () => void;
+					checkCallback?: (checking: boolean) => void;
+				};
+			};
+		};
+	}
 
-  interface Editor {
-    cm?: EditorView;
-  }
+	interface Editor {
+		cm?: EditorView;
+	}
 
-  interface Vault {
-    getConfig(id: string): string;
-  }
+	interface Vault {
+		getConfig(id: string): string;
+	}
 }
